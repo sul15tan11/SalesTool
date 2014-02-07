55cf9ec68b0b9fee4553cc60ac02c672eaeabcae
psxsa16 <psxsa16@ppshorizon1109.ad.nottingham.ac.uk>
2014-02-07 16:34:20 +0000

    Initial chekin of sales tool.

---
+++ SalesTool1/src/ca/demo/salestool1/Main.java
@@ -0,0 +1,22 @@
+package ca.demo.salestool1;
+
+public class Main {
+	
+
+		public static void main( String[] args){
+			
+			SalesData data = new SalesData();
+			
+			
+			displayGreeting();
+			data.display();
+		}
+
+		private static void displayGreeting() {
+			// TODO Auto-generated method stub
+			
+			System.out.println(" HELLOW  HAPPY SALES PEOPLE! ");
+			System.out.println(" THIS APP SHOWS SALES DATA");
+		}
+
+}
---
+++ SalesTool1/src/ca/demo/salestool1/SalesData.java
@@ -0,0 +1,13 @@
+package ca.demo.salestool1;
+
+public class SalesData {
+
+	int data[] = { 0, 4, 40 };
+
+	public void display() {
+
+		System.out.println(" TODO: Implementdata display");
+
+	}
+
+}
